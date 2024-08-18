8de6c9896ccfe7393b19d87e56dec092bff2253a
EstebanMqz <emarquez1895@gmail.com>
2024-08-18 15:11:26 -0600

    dotnet.bat & INI.BAT json xml, INI files & WinForms

--- .bat/INI.bat
+++ .bat/INI.bat
@@ -1,15 +1,17 @@
-@echo off
+@echo on
+:: This script will search for all .ini in C:\ and write PATHs to a csv subdir.
 setlocal enabledelayedexpansion
 
 REM Create the csv subdirectory if it doesn't exist
 if not exist ..\csv mkdir ..\csv
+    echo URL > ..\csv\ini_files.csv
 
-REM Initialize the CSV file in the csv subdirectory
-echo URL > ..\csv\ini_files.csv
-
-REM Search for .ini files and append their paths and URLs to the CSV file
-for /r C:\ %%f in (*.ini) do (
-    echo file://%%f >> ..\csv\ini_files.csv
-)
+    REM Search for .ini files and append their paths and URLs to the CSV file
+    for /r C:\ %%f in (*.ini) do (
+        echo file://%%f >> ..\csv\ini_files.csv
+        timeout /t 3 /nobreak >nul
+    )
+    
 echo '.ini files written to .csv\ini_files.csv'
-exit 
+exit /b 0
+
